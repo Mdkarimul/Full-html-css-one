94d2f21d102a50b79df0f2b4faee82dc791e8210
Mdkarimul <7mdkarimul@gmail.com>
2024-12-12 14:27:18 +0530

    adding swipper js java script code

--- assets/js/main.js
+++ assets/js/main.js
@@ -131,15 +131,26 @@ modalCloseBtn.addEventListener('click',()=>{
       portfolioBackDrop.classList.remove('active');
       portfolioModal.classList.remove('active');
    },100)
-
 })
-
-
 })
 
 /* =====================================================
    Testimonial Swiper
 ===================================================== */
+
+var swiper = new Swiper(".mySwiper", {
+   slidesPerView: 1,
+   spaceBetween: 30,
+   loop: true,
+   pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+   },
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+ });
 
 /* =====================================================
    Send/Receive emails from contact form - EmailJS
